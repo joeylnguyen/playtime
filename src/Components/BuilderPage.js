8470fc9faf80e09af8fe014125e411bad53d4e7f
Joey Nguyen <joeylnguyen95@gmail.com>
2020-06-08 05:36:49 -0700

    Create request handlers to get tracks

--- src/Components/BuilderPage.js
+++ src/Components/BuilderPage.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const BuilderPage = ( {userData} ) => {
+const BuilderPage = ( { userData, accessToken } ) => {
+  const [tracks, setTracks] = useState()
+
+  const handleCreatePlaylist = (playlistName) => {
+    axios({
+      method: 'get',
+      url: `https://api.spotify.com/v1/users/${userData.id}/playlists`,
+      data: {
+        name: playlistName,
+        description: 'Created with the PlayTime App!'
+      }
+    })
+  }
+
+  useEffect(() => {
+    const getTracks = (artistIds) => {
+      axios({
+        method: 'get',
+        url: `https://api.spotify.com/v1/recommendations?seed_artists=${artistIds}&limit=100`,
+        headers: {'Authorization': 'Bearer ' + accessToken},
+      })
+      .then((result) => console.log(result))
+      .catch((error) => console.log(error));
+    }
+
+    axios({
+      method: 'get',
+      url: 'https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=5',
+      headers: {'Authorization': 'Bearer ' + accessToken},
+    })
+      .then((result) => {
+        const artistsArray = result.data.items;
+        if (artistsArray.length) {
+          const artistIdString = artistsArray.map((artist) => artist.id).join(',');
+          getTracks(artistIdString)
+        }
+      })
+      .catch((error) => console.log(error))
+
+  }, [accessToken]);
+
   return (
-    <div>Hi {userData.display_name}</div>
+    <div>
+      <div>Hi {userData.display_name}</div>
+    </div>
+
   )
 };
 
