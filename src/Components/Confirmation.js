56dcd2decc9434d41e5c77f2851603943d6cdd00
Joey Nguyen <joeylnguyen95@gmail.com>
2020-06-09 03:08:09 -0700

    Add styling to confirmation modal

--- src/Components/Confirmation.js
+++ src/Components/Confirmation.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
 const Confirmation = ({ playlistData, accessToken, setShowModal }) => {
   const { id, uri } = playlistData;
@@ -20,25 +21,77 @@ const Confirmation = ({ playlistData, accessToken, setShowModal }) => {
   }, [accessToken, id])
 
   return (
-    <div>
-      {loading ? 'Creating playlist...' :
-      (<div>
-        <h3>Woot!</h3>
-        <p>Your playlist is ready for groovin'!</p>
-        <div>
-          <a href={uri}>
-            <img src={playlistImage} alt='Playlist cover' />
-          </a>
-        </div>
-        <div>
-          <a href={uri}>
-            <button>Open Playlist</button>
-          </a>
-          <button onClick={() => setShowModal(false)}>Close</button>
-        </div>
-      </div>)}
-    </div>
+    <Modal>
+      <ModalContent>
+        {loading ? 'Creating playlist...' :
+        (<div>
+          <h3>Woot!</h3>
+          <p>Your playlist is ready for groovin'!</p>
+          <div>
+            <a href={uri}>
+              <PlaylistImage src={playlistImage} alt='Playlist cover' />
+            </a>
+          </div>
+          <ButtonsWrapper>
+            <a href={uri}>
+              <OpenButton>Open Playlist</OpenButton>
+            </a>
+            <CloseButton onClick={() => setShowModal(false)}>Close</CloseButton>
+          </ButtonsWrapper>
+        </div>)}
+      </ModalContent>
+    </Modal>
   );
 };
 
 export default Confirmation;
+
+const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.6);
+`;
+
+const ModalContent = styled.div`
+  position: fixed;
+  background: white;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 1.5em 3.5em 2em;
+  border-radius: .5em;
+  text-align: center;
+`;
+
+const PlaylistImage = styled.img`
+  border-radius: .5em;
+`;
+
+const ButtonsWrapper = styled.div`
+  padding: 1em .25em .25em;
+  height: 50px;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  height: 100%;
+  cursor: pointer;
+  border-color: transparent;
+  font-weight: bolder;
+`;
+
+const OpenButton = styled(Button)`
+  width: 55%;
+  margin: .5em;
+  background-color: rgb(0,136,122);
+  color: white;
+`;
+
+const CloseButton = styled(Button)`
+  width: 35%;
+  margin: .5em;
+  background-color: rgb(237,234,229);
+`;
